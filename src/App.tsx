b64a1a42fcf0a94bbd4a6ca7cf33efd98f3f38bf
Matheus <theus.sousaa@gmail.com>
2025-12-18 12:30:01 -0300

    finished project

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import './App.scss';
 import Carousel from './components/Carousel';
 
 interface State {
+  itemWidth: number;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
   images: string[];
 }
 
@@ -20,17 +24,97 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = event.target;
+
+    this.setState(prev => ({ ...prev, [name]: value }));
   };
 
   render() {
-    const { images } = this.state;
+    const { images, itemWidth, frameSize, step, animationDuration } =
+      this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
+
+        <Carousel
+          images={images}
+          itemWidth={itemWidth}
+          frameSize={frameSize}
+          step={step}
+          animationDuration={animationDuration}
+        />
+
+        <div className="carousel-settings">
+          <div className="carousel-settings__field">
+            <label
+              htmlFor="itemWidth"
+              className="carousel-settings__field-name"
+            >
+              Tamanho da Imagem
+            </label>
+            <input
+              className="carousel-settings__input"
+              name="itemWidth"
+              type="number"
+              value={itemWidth}
+              onChange={this.handleChange}
+            ></input>
+          </div>
+          <div className="carousel-settings__field">
+            <label
+              htmlFor="frameSize"
+              className="carousel-settings__field-name"
+            >
+              Tamanho do carrossel
+            </label>
+            <input
+              className="carousel-settings__input"
+              name="frameSize"
+              type="number"
+              value={frameSize}
+              onChange={this.handleChange}
+            ></input>
+          </div>
+          <div className="carousel-settings__field">
+            <label htmlFor="step" className="carousel-settings__field-name">
+              Quantas imagens passar
+            </label>
+            <input
+              className="carousel-settings__input"
+              name="step"
+              type="number"
+              value={step}
+              onChange={this.handleChange}
+            ></input>
+          </div>
+          <div className="carousel-settings__field">
+            <label
+              htmlFor="animationDuration"
+              className="carousel-settings__field-name"
+            >
+              Duração da animação
+            </label>
+            <input
+              className="carousel-settings__input"
+              name="animationDuration"
+              type="number"
+              value={animationDuration}
+              onChange={this.handleChange}
+            ></input>
+          </div>
+
+          <input type="submit" name="" id="" />
+        </div>
       </div>
     );
   }
